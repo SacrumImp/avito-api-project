e044006f1dcce83168232a5c4554c3b46c1b44fc
Vladislav Alpeev <valpeev2000@gmail.com>
2024-08-19 00:07:07 +0300

    added statuses for flats and fixed matching keys and foreign keys

--- database/init.sql
+++ database/init.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS user_account (
     id SERIAL PRIMARY KEY,
     email VARCHAR(254) NOT NULL,
     password_hash VARCHAR(50) NOT NULL,
-    user_type_id SMALLINT NOT NULL REFERENCES user_type(id) ON DELETE CASCADE
+    user_type_id INTEGER NOT NULL REFERENCES user_type(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS developer (
@@ -19,16 +19,22 @@ CREATE TABLE IF NOT EXISTS house (
     id SERIAL PRIMARY KEY,
     address Text UNIQUE NOT NULL,
     year_of_construction SMALLINT NOT NULL,
-    developer_id SMALLINT REFERENCES developer(id) ON DELETE CASCADE,
+    developer_id INTEGER REFERENCES developer(id) ON DELETE CASCADE,
     inserted_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     last_flat_added_at TIMESTAMPTZ
 );
 
+CREATE TABLE IF NOT EXISTS status (
+    id SERIAL PRIMARY KEY,
+    title Text UNIQUE NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS flat (
-    house_id INTEGER UNIQUE REFERENCES house(id) ON DELETE CASCADE,
+    house_id INTEGER REFERENCES house(id) ON DELETE CASCADE,
     number SMALLINT,
     price BIGINT NOT NULL,
     number_of_rooms SMALLINT NOT NULL,
+    status_id INTEGER NOT NULL REFERENCES status(id) ON DELETE CASCADE,
     PRIMARY KEY (house_id, number)
 );
 
